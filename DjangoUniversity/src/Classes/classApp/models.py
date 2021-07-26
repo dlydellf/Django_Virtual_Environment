a00cbf7568acba504c3ef17962fee5ba4f46a105
DForeman <dlydellf@hotmail.com>
2021-07-26 17:01:05 -0400

    Set the objects' name(Title); commented code

--- DjangoUniversity/src/Classes/classApp/models.py
+++ DjangoUniversity/src/Classes/classApp/models.py
@@ -1,3 +1,6 @@
+# these entries will be translated into db tables
+# pass in 'models.Model' for EVERY created class
+# use 'models.CharField/TextField/etc' when defining fields
 from django.db import models
 
 # Create your models here.
@@ -6,3 +9,8 @@ class DjangoClasses(models.Model):
     Course_Number = models.IntegerField()
     Instructor_Name = models.CharField(max_length=60, blank=False, null=False)
     Duration = models.FloatField()
+
+    objects = models.Manager() # sets the object's name ('Title', in this case):
+
+    def __str__(self): # sets the object's Title:
+        return self.Title
