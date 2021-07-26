8f2015825e48917e7e4b9e7c04f087a044c4dc9f
DForeman <dlydellf@hotmail.com>
2021-07-26 19:54:11 -0400

    Added required transaction types to models.py

--- Django_Checkbook/src/DjangoCheckbook/Checkbook/models.py
+++ Django_Checkbook/src/DjangoCheckbook/Checkbook/models.py
@@ -14,11 +14,15 @@ class Account(models.Model):
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
+
+TransactionTypes = [('Deposit', 'Deposit'), ('Withdrawal', 'Withdrawal')]
+
+
 class Transaction(models.Model):
     date = models.DateField()
     type = models.CharField(max_length=10, choices=TransactionTypes)
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     description = models.CharField(max_length=100)
-    account = models.ForeignKey(Account ,on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
     Transactions = models.Manager()
