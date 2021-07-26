91ec8507ba1436cc88f77c9415a9e56c63eeb884
DForeman <dlydellf@hotmail.com>
2021-07-26 18:27:59 -0400

    Initial commit for the 'Django_Checkbook' project

---
+++ Django_Checkbook/src/DjangoCheckbook/Checkbook/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [ # stores the routes/paths within the project; the WHEN what's in 'views/py' gets displayed
+    path('', views.home, name='index'),
+    path('create/', views.create_account, name='create'),
+    path('balance/', views.balance, name='balance'),
+    path('transaction/', views.transaction, name='transaction'),
+]
