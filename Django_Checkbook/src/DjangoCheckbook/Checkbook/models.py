a6420c56b06e45a9f61f855b38384b5ac7b325a8
DForeman <dlydellf@hotmail.com>
2021-07-26 19:12:36 -0400

    Coded the 'Transaction' model

--- Django_Checkbook/src/DjangoCheckbook/Checkbook/models.py
+++ Django_Checkbook/src/DjangoCheckbook/Checkbook/models.py
@@ -13,3 +13,12 @@ class Account(models.Model):
     # as the owner's name isn' the primary key
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+class Transaction(models.Model):
+    date = models.DateField()
+    type = models.CharField(max_length=10, choices=TransactionTypes)
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    description = models.CharField(max_length=100)
+    account = models.ForeignKey(Account ,on_delete=models.CASCADE)
+
+    Transactions = models.Manager()
