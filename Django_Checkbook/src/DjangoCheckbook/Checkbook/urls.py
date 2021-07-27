fe877f87d43efe7df7674280c78d0867c9475351
DForeman <dlydellf@hotmail.com>
2021-07-27 19:47:23 -0400

    Updated BalanceSheet's url to expect a pk

--- Django_Checkbook/src/DjangoCheckbook/Checkbook/urls.py
+++ Django_Checkbook/src/DjangoCheckbook/Checkbook/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [ # stores the routes/paths within the project; the WHEN what's in 'views/py' gets displayed
     path('', views.home, name='index'),
     path('create/', views.create_account, name='create'),
-    path('balance/', views.balance, name='balance'),
+    path('<int.pk>/balance/', views.balance, name='balance'),
     path('transaction/', views.transaction, name='transaction'),
 ]
